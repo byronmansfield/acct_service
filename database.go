33e15af567a91bd076660d6d9a91058176768b08
Byron Mansfield <byron@byronmansfield.com>
2015-06-29 21:03:55 -0700

    Begins work on postgres connection

--- database.go
+++ database.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+const (
+	DB_USER     = "postgres"
+	DB_PASSWORD = "postgress"
+	DB_NAME     = "accounts"
+)
 
 var currentId int
 
@@ -9,6 +19,19 @@ var todos Todos
 func init() {
 	RepoCreatedTodo(Todo{Name: "Write presentation"})
 	RepoCreatedTodo(Todo{Name: "Host meetup"})
+}
+
+func SetupDB() *sql.DB {
+	dbinfo := fmt.Sprintf("user=%s pasword=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
+	db, err := sql.Open("postres", dbinfo)
+	PanicIf(err)
+	defer db.Close()
+}
+
+func PanicIf(err, error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 func RepoFindTodo(id int) Todo {
