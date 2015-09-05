33fa4e53d4c2cce1bdc14111e802029685681cea
Byron Mansfield <byron@byronmansfield.com>
2015-09-05 16:54:57 -0700

    Adds working get and post to psql db

--- database.go
+++ database.go
@@ -2,60 +2,34 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/lib/pq"
-)
-
-const (
-	DB_USER     = "postgres"
-	DB_PASSWORD = "postgress"
-	DB_NAME     = "accounts"
+	"os"
 )
 
 var currentId int
 
 var todos Todos
 
-func init() {
-	RepoCreatedTodo(Todo{Name: "Write presentation"})
-	RepoCreatedTodo(Todo{Name: "Host meetup"})
+func dbConnect() *sql.DB {
+	datname := os.Getenv("PGDATABASE")
+	sslmode := os.Getenv("PGSSLMODE")
+
+	if datname == "" {
+		os.Setenv("PGDATABASE", "tasks")
+	}
+
+	if sslmode == "" {
+		os.Setenv("PGSSLMODE", "disable")
+	}
+
+	conn, err := sql.Open("postgres", "")
+	checkErr(err)
+
+	return conn
 }
 
-func SetupDB() *sql.DB {
-	dbinfo := fmt.Sprintf("user=%s pasword=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
-	db, err := sql.Open("postres", dbinfo)
-	PanicIf(err)
-	defer db.Close()
-}
-
-func PanicIf(err, error) {
+func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func RepoFindTodo(id int) Todo {
-	for _, t := range todos {
-		if t.Id == id {
-			return t
-		}
-	}
-	return Todo{}
-}
-
-func RepoCreatedTodo(t Todo) Todo {
-	currentId += 1
-	t.Id = currentId
-	todos = append(todos, t)
-	return t
-}
-
-func RepoDestroyTodo(id int) error {
-	for i, t := range todos {
-		if t.Id == id {
-			todos = append(todos[:i], todos[i+1:]...)
-			return nil
-		}
-	}
-	return fmt.Errorf("Could not find Todo with id of %d to delete", id)
 }
